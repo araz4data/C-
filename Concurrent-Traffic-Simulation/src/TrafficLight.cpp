1bfc197921addf013022a3804b754eddf3407e86
araz4data <38193052+araz4data@users.noreply.github.com>
2021-11-18 09:25:49 -0800

    Adding src code

---
+++ Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <random>
+#include <time.h>
+#include "TrafficLight.h"
+
+/* Implementation of class "MessageQueue" */
+
+template <typename T>
+T MessageQueue<T>::receive()
+{
+    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // to wait for and receive new messages and pull them from the queue using move semantics. 
+    // The received object should then be returned by the receive function. 
+
+    std::unique_lock<std::mutex> lck( _mtx ) ;
+    _cond.wait( lck ) ;
+
+    T message = std::move( _queue.back() ) ;
+    _queue.pop_back() ;
+
+    return message ;
+}
+
+template <typename T>
+void MessageQueue<T>::send( T &&message )
+{
+    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // as well as _condition.notify_one() to add a new message to the queue 
+    // and afterwards send a notification.
+
+    
+    std::lock_guard<std::mutex> lck( _mtx ) ;
+
+    std::cout << "   Message #" << message << " has been sent to the queue" << std::endl ;
+    _queue.push_back( std::move( message ) ) ;
+    _cond.notify_one() ;
+}
+
+
+/* Implementation of class "TrafficLight" */
+
+ 
+TrafficLight::TrafficLight()
+{
+    _currentPhase = TrafficLightPhase::red;
+}
+
+void TrafficLight::waitForGreen()
+{
+    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
+    // runs and repeatedly calls the receive function on the message queue. 
+    // Once it receives TrafficLightPhase::green, the method returns.
+    while ( true ) {
+        TrafficLightPhase lightPhaseMessage = _messageQueue.receive();
+        if ( lightPhaseMessage == TrafficLightPhase::green ) return ;
+    }
+}
+
+TrafficLightPhase TrafficLight::getCurrentPhase()
+{
+    return _currentPhase;
+}
+
+void TrafficLight::simulate()
+{
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started 
+    // in a thread when the public method „simulate“ is called. To do this, use 
+    // the thread queue in the base class. 
+    
+    _threads.emplace_back( std::thread(&TrafficLight::cycleThroughPhases, this ) ) ;
+
+}
+
+// virtual function which is executed in a thread
+void TrafficLight::cycleThroughPhases()
+{
+    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
+    // and toggles the current phase of the traffic light between red and green and sends an update method 
+    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+
+    
+    // random number  between 4000 to 6000
+    std::default_random_engine generator ;
+    std::uniform_real_distribution<double> distribution( 4000.0, 6000.0 ) ;
+    
+    // one cycle duration 
+    double cycDur = distribution( generator ) ;
+
+    // istart the chronometer
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate ;
+    lastUpdate = std::chrono::system_clock::now() ;
+
+    while ( true ) {
+
+        // sleep for 1ms
+        std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) ) ;
+
+        // calc time difference
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+        if ( timeSinceLastUpdate >= cycDur ) {
+            // toggle traffic phase
+            if ( _currentPhase == TrafficLightPhase::green ) 
+                _currentPhase =  TrafficLightPhase::red ;
+            else
+                _currentPhase = TrafficLightPhase::green ;
+
+            // send update message to queue
+            _messageQueue.send( std::move(_currentPhase) ) ;
+
+            // reset chronometer for next cycle
+            lastUpdate = std::chrono::system_clock::now() ;
+
+            distribution( generator ) ;
+
+        }
+
+    }
+}
